74f1ff294f6ab88e93441886474e6c201f8cc17c
Anke <pfeiffer.anke@outlook.com>
2021-10-12 12:04:47 +0200

    P element appears after clicking on the button

--- main.js
+++ main.js
@@ -1,31 +1,52 @@
-const correctAnswer = true;
+const correctAnswer = false;
 
 const myQuestion = document.querySelector(".question");
 myQuestion.textContent = "Are you a robot?";
 
-document.body.onload = addElement;
-function addElement() {
-  const newP = document.createElement("p");
-  const newAnswer = document.createTextNode("You are perfectly right!");
-  newP.appendChild(newAnswer);
-  const buttons = document.getElementById("anker");
-  document.body.insertBefore(newP, buttons);
-}
+// document.body.onload = answerYes;
+// function answerYes() {
+//   const newP = document.createElement("p");
+//   const newAnswer = document.createTextNode("You are perfectly right!");
+//   newP.appendChild(newAnswer);
+//   const buttons = document.getElementById("anker");
+//   document.body.insertBefore(newP, buttons);
+// }
 
 const yesButton = document.querySelector(".yes");
 yesButton.onclick = function () {
   if (correctAnswer === true) {
-    alert("You are perfectly right!");
+    const newP = document.createElement("p");
+    newP.textContent = "You are perfectly right!";
+    newP.className = "correct";
+    document.body.append(newP);
   } else {
-    alert("Think about it one more time!");
+    const newP = document.createElement("p");
+    newP.textContent = "Think about it one more time!";
+    newP.className = "wrong";
+    document.body.append(newP);
   }
 };
 
 const noButton = document.querySelector(".no");
 noButton.onclick = function () {
   if (correctAnswer === false) {
-    alert("You are perfectly right!");
+    const newP = document.createElement("p");
+    newP.textContent = "You are perfectly right!";
+    newP.className = "correct";
+    document.body.append(newP);
   } else {
-    alert("Think about it one more time!");
+    const newP = document.createElement("p");
+    newP.textContent = "Think about it one more time!";
+    newP.className = "wrong";
+    document.body.append(newP);
   }
 };
+
+// const noButton = document.querySelector(".no");
+// noButton.onclick = function () {
+//   if (correctAnswer === false) {
+//     alert("You are perfectly right!");
+//   } else {
+//     alert("Think about it one more time!");
+//   }
+// };
