bc7dc29cb40620103466b0490546bd65da6b331b
Anke <pfeiffer.anke@outlook.com>
2021-10-12 11:05:06 +0200

    Create p element in JavaScript

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ const correctAnswer = true;
 const myQuestion = document.querySelector(".question");
 myQuestion.textContent = "Are you a robot?";
 
+document.body.onload = addElement;
+function addElement() {
+  const newP = document.createElement("p");
+  const newAnswer = document.createTextNode("You are perfectly right!");
+  newP.appendChild(newAnswer);
+  const buttons = document.getElementById("anker");
+  document.body.insertBefore(newP, buttons);
+}
+
 const yesButton = document.querySelector(".yes");
 yesButton.onclick = function () {
   if (correctAnswer === true) {
